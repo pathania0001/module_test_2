f284280ee320ac7c71437fbff8ad3762d5df381c
pathania0001 <83996978+pathania0001@users.noreply.github.com>
2025-07-19 12:23:08 +0530

    solution

---
+++ script.js
@@ -0,0 +1,45 @@
+/** @format */
+
+let arr = [
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin" },
+];
+
+function PrintDeveloperbyMap() {
+  arr.filter(employee =>employee.profession === "develper")
+  .map( employee => console.log("Developers :",employee))
+}
+
+function PrintDeveloperbyForEach() {
+  arr.forEach( employee =>  employee.profession === "developer" && console.log("Developers :",employee));
+}
+
+function addData() {
+ const newEmployeeArray =  {id:4,name:"susan",age:"20",profession:"intern"} 
+ const isExist = arr.some( employee => employee.id === newEmployeeArray.id)
+ if(isExist){
+  console.log("Already Existed")
+  return;
+ }
+ arr.push(newEmployee);
+ console.log("Updated Array :",arr);
+}
+
+function removeAdmin() {
+  const newArray = arr.filter( employee => employee.profession !== "admin")
+  console.log("Updated Array :",newArray);
+  return newArray;
+}
+
+function concatenateArray() {
+   const newEmployees = [
+    { id: 5, name: "Rajat", age: "24", profession: "Software Engineer" },
+    { id: 6, name: "Anoop", age: "25", profession: "developer" },
+    { id: 7, name: "Vineet", age: "24", profession: "tester" }
+  ];
+
+  const Updated = arr.concat(newEmployees);
+  console.log("Updated :",Updated);
+  return Updated;
+}
